7624cbbd3225b3120accbd4755ac5e76addb0b9d
hasnainshah143 <hasnainshah143@gmail.com>
2025-07-03 04:43:50 +0500

    Fix: defined app using express

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('views'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
 });
